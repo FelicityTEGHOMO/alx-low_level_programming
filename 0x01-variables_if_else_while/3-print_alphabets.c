c27396e19c534e45350861c823b95d411592fe7a
felicity <feliciteteghomo@gmail.com>
2022-03-12 06:23:00 -0800

    Print the alphabet in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase.
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 97 + 25; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i <= 65 + 25; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
