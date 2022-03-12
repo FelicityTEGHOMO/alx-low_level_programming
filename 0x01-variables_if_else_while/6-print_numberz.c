02fb13cf9e7e96be877200322fc029b38453a911
felicity <feliciteteghomo@gmail.com>
2022-03-12 06:38:53 -0800

    Print all digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all digit numbers of base 10.
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 48 + 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
