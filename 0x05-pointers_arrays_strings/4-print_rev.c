6cfd79c0be502b0fc814c73c2f7da70e1e212875
Felicity <feliciteteghomo@gmail.com>
2022-03-25 16:07:08 +0100

    Print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+/**
+  * print_rev - print a string in reverse
+  * @s: the string
+  */
+void print_rev(char *s)
+{
+	int size = 0;
+	int i;
+
+	while (*(s + size))
+	{
+		size++;
+	}
+	for (i = size; i <= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
